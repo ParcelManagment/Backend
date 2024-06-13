dd07d2b921307ed08fb72aecdde3c9c8210d68f8
Kavindu Wickramasekara <kavi2020wick@gmail.com>
2024-06-13 22:43:22 +0530

    add : packages and front

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
 const port = 3000
+
+
+const app = express()
+app.use(express.static('public'))
+app.set('view engine','ejs')
+
+const dbURI = "###############"
+mongoose.connect(dbURI,{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true})
+.then((result)=>app.listan(port))
+.catch((err)=>console.log(err));
+
 
 app.get('/', (req, res) => {
   res.send('entry point')
 })
-
+app.get('/login', (req, res) => {
+  res.send('This is login')
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
