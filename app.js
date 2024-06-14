985e5b1e0c8445d606796bfed118d58fa8167900
Kavindu Wickramasekara <kavi2020wick@gmail.com>
2024-06-14 09:44:30 +0530

    add: successful mongodb connection

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const mongoose = require('mongoose')
+require('dotenv').config()
 const port = 3000
 
 
 const app = express()
+
 app.use(express.static('public'))
 app.set('view engine','ejs')
 
-const dbURI = "mongodb+srv://kavi2020wick:<password>@cluster0.kh6gmvi.mongodb.net/"
-mongoose.connect(dbURI,{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true})
-.then((result)=>app.listan(port))
+const dbURI = `mongodb+srv://kavi2020wick:${process.env.PASSWORD}@cluster0.kh6gmvi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+console.log(dbURI);
+//mongoose.connect(dbURI,{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true})
+mongoose.connect(dbURI)
+.then((result)=>console.log("Database connetion success"))
 .catch((err)=>console.log(err));
 
 
