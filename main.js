4b6eb51cf21ad9827de31a61a721d9429f8f3233
Sanuda <sanudatharin@gmail.com>
2024-07-10 20:50:09 +0530

    Updated of the code

--- main.js
+++ main.js
@@ -1,59 +1,39 @@
-const express = require("express")
-const mysql = require("mysql")
-const app = express()
+const express = require("express");
+const mysql = require("mysql2");
+const app = express();
 
 const con = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:null,
-    database:"parcel"
-})
+    host: "localhost",
+    user: "root",
+    password: null,
+    database: "parcel"
+});
 
-con.connect((err)=>{
-
-    if(err)
-    {
-
-        console.log(err)
-            
+con.connect((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("CONNECTED!");
     }
+});
 
-    else
-    {
-        console.log("CONNECTED!")
+app.get("/fetchbyid/:id", (req, res) => {
+    const fetchid = req.params.id;
+
+    con.query("SELECT name FROM mytable WHERE id=?", [fetchid], function(err, result, fields) {
+        if (err) {
+            console.log(err);
+            res.status(500).send(err);  // Send error response if there is an error
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+app.listen(3000, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("On port 3000");
     }
-
-})
-
-app.get("/fetchbyid/:id",(req,res)=>{
-
-    const fetchid=req.params.id;
-
-    con.query("select name from mytable where id=?",fetchid,function(err,result,fields){
-        if(err)
-        {
-            console.log(err)
-        }
-        else
-        {
-            res.send(result)
-        }
-    })
-
-})
-
-app.listen(3000,(err)=>{
-
-    if(err)
-        {
-            console.log(err)
-        }
-        else
-        {
-
-            console.log("On port 3000")
-        
-        }
-
-
-})
+});
